2a354fdb7f5d65d72b4668e83e45017d150d7274
Mavrov, Sergii <sergii.mavrov@hilti.com>
2022-10-27 11:27:27 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM php:7.2-apache
+COPY ./main.php /var/www/html/main.php
---
+++ main.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Opening HILTI Dashboard</title>
+    <style>
+        iframe {
+            width: 100%;
+            height: 500px;
+        }
+    </style>
+</head>
+<body>
+<iframe src="demo-page.html" name="dashboard"></iframe>
+<p><a href="https://jira.hilti.com" target="dashboard">Dashboard of HILTI</a></p>
+</body>
+</html>
+
+<?php
+echo("Hello, world");
+
+
+
+
