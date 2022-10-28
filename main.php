e5b5d359dd2a826b7744b0cd4b1aadcd2511386b
Mavrov, Sergii <sergii.mavrov@hilti.com>
2022-10-28 13:13:41 +0200

    scripts

--- main.php
+++ main.php
@@ -17,6 +17,19 @@
 
 <?php
 echo("Hello, world");
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, "https://jira.hilti.com/plugins/servlet/twofactor/public/pinvalidation?target=/");
+curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_POST);
+curl_setopt($curl, CURLOPT_POSTFIELDS, array("2fpin=121212&atl_token=%24atl_token&twofalogin=pinlogin"));
+curl_setopt($curl, CURLOPT_HEADER, true);
+$headers = array(
+    "Authorization: Bearer OTMxMDk2MzQxODE0Onc7z4Z82m0dIkoDn9+qvLAQptLF",
+    'Content-Type: multipart/form-data'
+);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+$result = curl_exec($curl);
 
 
 
